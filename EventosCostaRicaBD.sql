281420642b1dff40e8f5bbeac433e857d2ab6795
JasonChavarria07 <108899160+JasonChavarria07@users.noreply.github.com>
2025-07-07 16:32:11 -0600

    Modificación script SQL - Jason Chavarría

--- EventosCostaRicaBD.sql
+++ EventosCostaRicaBD.sql
@@ -30,3 +30,20 @@ ADD Apellidos NVARCHAR(100) NULL;
 -- Seed roles for Movie Theater
 INSERT INTO Roles (Nombre) VALUES ('Admin');
 INSERT INTO Roles (Nombre) VALUES ('Guest');
+
+--Creación de la tabla eventos si no se desea migrar desde Visual
+CREATE TABLE [dbo].[Eventos](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [nvarchar](100) NOT NULL,
+	[Descripcion] [nvarchar](300) NOT NULL,
+	[FechaInicio] [datetime2](7) NOT NULL,
+	[FechaFin] [datetime2](7) NOT NULL,
+	[Lugar] [nvarchar](100) NOT NULL,
+	[Precio] [decimal](18, 2) NOT NULL,
+	[ImagenUrl] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Eventos] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
